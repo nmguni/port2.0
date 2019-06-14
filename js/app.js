487434165e46fa8fd9f725c978fa891318914645
Ndumiso Mguni <Ndumiso29@gmail.com>
2019-06-14 14:08:01 -0400

    Updates

---
+++ js/app.js
@@ -0,0 +1,5 @@
+// mobile nav bar activation
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+});
